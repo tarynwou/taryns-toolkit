309ca1833ec8bf4ec1333e2b04100831d7164e33
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2022-02-02 21:21:49 -0800

    Prep for contact icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-6">
+          <div class="col-lg-5">
             <img
               // class="img-fluid rounded mb-4 mb-lg-0"
               className="headshot"
@@ -16,7 +16,7 @@ function Home() {
             />
           </div>
           <div class="col-lg-6">
-            <h1 class="font-weight-light">Home</h1>
+            <h1>Hi, I'm Taryn!</h1>
             <p>
               I’m a second-year business and computer science student at the
               University of British Columbia. I am always looking to pursue new
@@ -26,6 +26,8 @@ function Home() {
               <br></br>
               Let’s connect!
             </p>
+            {/* add IG + LI icons */}
+            <row></row>
           </div>
         </div>
       </div>
